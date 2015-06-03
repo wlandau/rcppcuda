f2a5b29d3986a9488ad929448e5501e75acdeed1
wlandau <will.landau@gmail.com>
2015-06-03 14:07:43 -0500

    added kernel code

--- src/rcppcuda.cu
+++ src/rcppcuda.cu
@@ -1,11 +1,19 @@
-#include <Rcpp.h>
+s#include <Rcpp.h>
 using namespace Rcpp;
+
+
+__global__ void mykernel(int a){
+  int id = threadIdx.x;
+  int b = a;
+}
+
 
 RcppExport SEXP rcppcuda(SEXP r) {
   S4 c(r);
   double *x = REAL(c.slot("x"));
-  int *y = INTEGER(c.slot("y"));
+  int *y = INTEGER(c.slot("y"));a.h
   x[0] = 500.0;
   y[1] = 1000;
+  mykernel<<<1, 1>>>(1);
   return R_NilValue;
 }
