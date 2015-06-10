5ee1714f6de946e22dca5db24ece7ddd6283420a
wlandau <will.landau@gmail.com>
2015-06-10 17:05:22 -0500

    created deliberate memory leak so i could figure out cuda-memcheck

--- src/someCUDAcode.c
+++ src/someCUDAcode.c
@@ -1,3 +1,9 @@
+#include <R.h>
+
+#define CUDA_CALL(x) {if((x) != cudaSuccess){ \
+  REprintf("CUDA error at %s:%d\n",__FILE__,__LINE__); \
+  REprintf("  %s\n", cudaGetErrorString(cudaGetLastError()));}}
+
 __global__ void mykernel(int a){
   int id = threadIdx.x;
   int b = a;
@@ -6,5 +12,7 @@ __global__ void mykernel(int a){
 }
 
 extern "C" void someCUDAcode() {
+  int a;
+  CUDA_CALL(cudaMalloc((void**) &a, sizeof(int)));
   mykernel<<<1, 1>>>(1);
 }
